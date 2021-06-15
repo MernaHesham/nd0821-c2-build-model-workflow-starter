ebacf51ce305d9e6aa01417ae1b8a198b58939f4
Merna Hesham <mernahesham97@yahoo.com>
2021-06-15 15:46:47 +0200

    added data_check

--- main.py
+++ main.py
@@ -64,10 +64,18 @@ def go(config: DictConfig):
                 )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['src_repository']}/data_check",
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "output_description": "Data_with_outliers_and_null_values_removed",
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                    }
+                )
 
         if "data_split" in active_steps:
             ##################
