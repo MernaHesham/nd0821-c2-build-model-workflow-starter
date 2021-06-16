80a1bf8fa6dd75cb45f274b87eafc4c3e8ebb3ef
Merna Hesham <mernahesham97@yahoo.com>
2021-06-16 10:16:15 +0200

    fixing list indices must be int or slices changed variable name from random_Forest to random_forest in sk_pipe creation in inference pipeline

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -235,7 +235,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # ColumnTransformer instance that we saved in the `preprocessor` variable, and a step called "random_forest"
     # with the random forest instance that we just saved in the `random_forest` variable.
     # HINT: Use the explicit Pipeline constructor so you can assign the names to the steps, do not use make_pipeline
-    sk_pipe = Pipeline([('preprocess', preprocessor), ('random_Forest', random_Forest)])
+    sk_pipe = Pipeline([('preprocess', preprocessor), ('random_forest', random_Forest)])
 
     return sk_pipe, processed_features
 
