6b7e149acc6b90ce4090488c5a6b8635d135ae86
Merna Hesham <mernahesham97@yahoo.com>
2021-06-30 13:21:54 +0200

    saving the mae too to the summary

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -134,6 +134,7 @@ def go(args):
     ######################################
     # Here we save r_squared under the "r2" key
     run.summary['r2'] = r_squared
+    run.summary['mae'] = mae
     # Now log the variable "mae" under the key "mae".
     run.log({"r2": r_squared, "mae": mae})
     ######################################
